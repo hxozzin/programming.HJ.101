d19dd0e9c8956913190b8b53f70cc3130ab34298
Kim ho jin <112994126+hxozzin@users.noreply.github.com>
2022-09-21 10:51:23 +0900

    2
    2

---
+++ 2nd_week/Prob_2/prob_2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+	printf("Enter characters : %c\n", 'A');
+	ch = 'A';
+	printf("The ASCII code value of the %c character is 65.\n", ch);
+
+	return 0;
+}
