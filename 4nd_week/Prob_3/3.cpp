907d0883cc1fcb2fc5626e5614ae0733f5406845
hxozzin <rlaghwls0922@naver.com>
2022-10-04 23:11:49 +0900

    hello world!
    hello world!

--- 4nd_week/Prob_3/3.cpp
+++ 4nd_week/Prob_3/3.cpp
@@ -3,40 +3,33 @@
 
 int main(void)
 {
-	int nA = 0; // 변수 A 초기화
-	int nB = 0; // 변수 B 초기화
+	double nA = 0; // 변수 A 초기화
+	double nB = 0; // 변수 B 초기화
+	double nRes;
 	char chOper; //사칙연산기호 Oper(+,-,*,/) 초기화
 
 	printf("Input your arithmetic poeration : "); //정수의 사칙연산 입력
-	scanf_s("%d%c%d", &nA, &chOper, 1, &nB); 
+	scanf_s("%lf%c%lf", &nA, &chOper, &nB); 
 
-	switch (chOper)
+	
+	if (chOper == '+') //덧셈 연산식
 	{
-	case '+': //덧셈 연산식
-		{
-		int nRes = nA + nB;
-		printf("%d %c %d = %d\n", nA, chOper, nB, nRes);
-		break;
-		}
-	case '-': // 뺄셈 연산식
-		{
-		int nRes = nA - nB;
-		printf("%d %c %d = %d\n", nA, chOper, nB, nRes);
-		break;
-		}
-	case '*': // 곱셈 연산식
-		{
-		int nRes = nA * nB;
-		printf("%d %c %d = %d\n", nA, chOper, nB, nRes);
-		break;
-		}
-	case '/': // 나눗셈 연산식
-		{
-		int nRes = nA / nB;
-		printf("%d %c %d = %d\n", nA, chOper, nB, nRes);
-		break;
-		}
+		double nRes = nA + nB;
 	}
+	else if (chOper == '-') // 뺄셈 연산식
+	{
+		double nRes = nA - nB;
+	}
+	else if (chOper == '*') // 곱셈 연산식
+	{
+		double nRes = nA * nB;
+	}
+	else (chOper == '/'); // 나눗셈 연산식
+		{
+		double nRes = nA / nB;
+		}
 
-	return 0;
+	printf("%d %c %d = %.2lf\n", nA, chOper, nB, nRes);
+
+ return 0;
 }
