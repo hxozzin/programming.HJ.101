36a11ec8b58b1b82f8853142f4d4db7c6783e862
Kim ho jin <112994126+hxozzin@users.noreply.github.com>
2023-03-10 14:46:48 +0900

    Update prob1.cpp

--- 12nd_week/아두이노_가변저항 LED 서보모터/prob1.cpp
+++ 12nd_week/아두이노_가변저항 LED 서보모터/prob1.cpp
@@ -16,7 +16,7 @@ int a, b;
 
 void setup()
 {
-	pinMode(nRedpin, OUTPUT);
+	pinMode(nRedpin, OUTPUT);	// 디지털 핀을 입력 또는 출력으로 설정
 	pinMode(nBluepin, OUTPUT);
 	pinMode(nGreenpin, OUTPUT);
 	pinMode(nYellowpin, OUTPUT);
@@ -36,13 +36,13 @@ void loop()
 
 
 
-	int ReadValue = analogRead(A0);
+	int ReadValue = analogRead(A0);	// 아날로그 핀에서 아날로그 값 읽음
 
-	if (digitalRead(nPushSwpin) == HIGH)
+	if (digitalRead(nPushSwpin) == HIGH)		// 디지털 핀에서 Pin값을 읽음
 	{
 		Serial.print("Switch:ON\n");
 
-		digitalWrite(nRedpin, HIGH);
+		digitalWrite(nRedpin, HIGH);	// High 또는 Low 값을 디지털 핀에 출력
 		Serial.print("RedLED : ON\n");
 		delay(ReadValue); // Wait for 1000 millisecond(s)
 		digitalWrite(nRedpin, LOW);
